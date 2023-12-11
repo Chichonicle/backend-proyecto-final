6d8537f8ff5082a2aa6e0422f1ef9648dba91aa6
Christian <christianperez.rbb@gmail.com>
2023-12-11 12:04:59 +0100

    update: update model of mensaje with relations to other tables.

--- app/Models/Mensaje.php
+++ app/Models/Mensaje.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Mensaje extends Model
 {
@@ -14,4 +16,13 @@ class Mensaje extends Model
         'user_id',
         'sala_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function salas(): HasMany
+    {
+        return $this->hasMany(Sala::class);
+    }
 }
