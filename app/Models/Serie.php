a857d771d974d3d7f21d0b414d1f1198d6e72719
Christian <christianperez.rbb@gmail.com>
2023-12-11 12:01:25 +0100

    update: update model of serie with relations to other tables.

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Serie extends Model
 {
@@ -16,4 +18,21 @@ class Serie extends Model
         'url',
         'picture',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function series(): BelongsTo
+    {
+        return $this->belongsTo(Serie::class);
+    }
+    public function salas(): HasMany
+    {
+        return $this->hasMany(Sala::class);
+    }
+    public function mensajes(): HasMany
+    {
+        return $this->hasMany(Mensaje::class);
+    }
 }
