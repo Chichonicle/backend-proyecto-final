30172498517cabd31c736da85ab8d145f857ccd3
Christian <christianperez.rbb@gmail.com>
2023-12-11 13:14:37 +0100

    feat: create register user endpoint.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        try {
+            $validator = $this->validateRegister($request);
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => $validator->errors(),
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            $newUser = User::create([
+                "name" => $request->input("name"),
+                "surname" => $request->input("surname"),
+                "username" => $request->input("username"),
+                "email" => $request->input("email"),
+                "password" => bcrypt($request->input("password")),
+
+            ]);
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "User created successfully",
+                    "data" => $newUser,
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error creating new user",
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
 }
