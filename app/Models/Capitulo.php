9d155a06c37796b6200fd3eac4ae673ad8e67105
Christian <christianperez.rbb@gmail.com>
2023-12-11 12:06:28 +0100

    update: update model of capitulo with relations to other tables.

--- app/Models/Capitulo.php
+++ app/Models/Capitulo.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Capitulo extends Model
 {
@@ -14,4 +15,9 @@ class Capitulo extends Model
         'url',
         'serie_id',
     ];
+
+    public function series(): HasMany
+    {
+        return $this->hasMany(Serie::class);
+    }
 }
