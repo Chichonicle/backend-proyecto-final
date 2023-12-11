83e6f292e47b89ba2a2929cc93be39e35e4f7794
Christian <christianperez.rbb@gmail.com>
2023-12-11 11:37:42 +0100

    feat: create migrations and Model of Users.

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('surname', 100);
+            $table->string('username', 50)->unique();
+            $table->string('email', 70);
+            $table->string('password', 200);
+            $table->enum('role', ['user','admin','superadmin'])->default('user');
+            $table->boolean('is_active')->default(true);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
