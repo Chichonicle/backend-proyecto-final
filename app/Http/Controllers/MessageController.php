9ab2b21ab9284aaff387ba1cea3cad930b359b01
Christian <christianperez.rbb@gmail.com>
2023-12-21 05:16:20 +0100

    feat: create controller to get message by sala.

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mensaje;
+use App\Models\Sala;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
@@ -93,6 +94,57 @@ class MessageController extends Controller
                 [
                     "success" => false,
                     "message" => "Error gettin messages"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    public function getMessage(Request $request)
+    {
+        try {
+
+            $user = auth()->user();
+            $sala_id = $request->input('salas_id');
+            $user_sala = Sala::query()->where('user_id', $user->id)->where('series_id', $sala_id)->first();
+
+            if (!$user_sala) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "You are not a member of this chat"
+                    ],
+                    Response::HTTP_OK
+                );
+            }
+
+            $salaChat = Mensaje::query()->where('salas_id', $sala_id)->get();
+
+            if ($salaChat->isEmpty()) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "This chat is void"
+                    ],
+                    Response::HTTP_OK
+                );
+            }
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Room chat obtained succesfully",
+                    "data" =>  $salaChat
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error obtaining a chat room"
                 ],
                 Response::HTTP_INTERNAL_SERVER_ERROR
             );
