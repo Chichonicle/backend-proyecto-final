7a1d1bed2c4152474b83293d1ea1d5930b140b03
Christian <christianperez.rbb@gmail.com>
2023-12-21 21:14:04 +0100

    feat: create controller to create series by admin.

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Series;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class adminController extends Controller
+{
+    public function createSerie(Request $request)
+    {
+        try {
+
+            $user = auth()->user();
+            if ($user->role != "admin") {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "You are not an admin"
+                    ],
+                    Response::HTTP_UNAUTHORIZED
+                );
+            }
+
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|min:3|max:100',
+                'genre' => 'required|min:3|max:50',
+                'year' => 'required|min:4|max:4',
+                'url' => 'required|min:3|max:200',
+                'picture' => 'required|min:3|max:200',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "success" => true,
+                        "message" => "Error creating serie",
+                        "error" => $validator->errors()
+                    ],
+                    Response::HTTP_BAD_REQUEST
+                );
+            }
+
+            $newSerie = Series::create(
+                [
+                    "name" => $request->input('name'),
+                    "genre" => $request->input('genre'),
+                    "year" => $request->input('year'),
+                    "url" => $request->input('url'),
+                    "picture" => $request->input('picture'),
+                    "user_id" => $user->id
+                ]
+            );
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Serie created successfully",
+                    "data" => $newSerie
+                ],
+                Response::HTTP_CREATED
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error creating serie"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\adminController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\SeriesController;
@@ -17,15 +18,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// PUBLIC
+// USERS
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
 
 // USER WITH AUTH
-Route::middleware("auth:sanctum")->get('/profile', [UserController::class, 'profile']);
-Route::middleware("auth:sanctum")->post('/logout', [UserController::class, 'logout']);
-Route::middleware("auth:sanctum")->put('/update', [UserController::class, 'updateProfile']);
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function () {
+Route::get('/profile', [UserController::class, 'profile']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::put('/update', [UserController::class, 'updateProfile']);
+});
 
 // SERIES PUBLIC
 Route::get('/series', [SeriesController::class, 'getAllSeries']);
@@ -58,4 +63,12 @@ Route::group([
 Route::post('/member', [MemberController::class, 'addUserSalas']);
 Route::post('/member/add', [MemberController::class, 'addMember']);
 Route::delete('/member/{id}', [MemberController::class, 'exitSalaById']);
+});
+
+
+//ADMIN
+Route::group([
+    'middleware' => ['auth:sanctum', 'admin']
+], function () {
+    Route::post('/serie', [adminController::class, 'createSerie']);
 });
