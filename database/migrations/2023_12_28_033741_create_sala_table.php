a2603c38cff22f402dd8c9eb443861102d6b3719
Christian <christianperez.rbb@gmail.com>
2023-12-29 04:01:48 +0100

    fix: fixed migration of sala the table name max 100.

--- database/migrations/2023_12_28_033741_create_sala_table.php
+++ database/migrations/2023_12_28_033741_create_sala_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('salas', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 20);
+            $table->string('name', 100);
             $table->unsignedBigInteger('series_id');
             $table->foreign('series_id')->references('id')->on('series');
             $table->timestamp('created_at')->useCurrent();
