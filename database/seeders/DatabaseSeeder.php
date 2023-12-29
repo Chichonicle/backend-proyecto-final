8a125794fa0ea4b210fb4b11e2f246e05ea29a8d
Christian <christianperez.rbb@gmail.com>
2023-12-29 03:55:03 +0100

    feat: create seeder of sala at database of seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
          $this->call([
           SeriesSeeder::class,
          ]);
+         $this->call([
+          SalaSeeder::class,
+         ]);
     }
 }
